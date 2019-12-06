2bd9b5000743766cab80f099ba4072000ff14164
JRtoman <jeremy.n.rotman@gmail.com>
2019-12-06 15:11:43 -0800

    Commented Code

--- pull_data.py
+++ pull_data.py
@@ -1,6 +1,6 @@
-import yaml
 import json
 import requests
+import os.path
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,22 +11,31 @@ def pull_shots():
     x = []
     y = []
     result = []
+    # Iterate over all 1271 games from the NHL season
     for i in range(1, 1272):
+        # Must use 4 digit code for each game, starting from 0001
         url = "https://statsapi.web.nhl.com/api/v1/game/201802{}/feed/live".format(str(i).zfill(4))
         r = requests.get(url)
         data = r.json()
 
+        # Ignore data on teams and players, just interested in events during game
         all_plays = data["liveData"]["plays"]["allPlays"]
         sog = []
         for play in all_plays:
+            # SHOT event is a shot that was saved
             if play["result"]["eventTypeId"] == "SHOT":
                 sog.append(play)
+            # GOAL event is a shot that resulted in a goal
             elif play["result"]["eventTypeId"] == "GOAL":
                 if "emptyNet" in play["result"]:
+                    # Do not want to include empty netters, since they cannot be saved by goalie
                     if play["result"]["emptyNet"] is False:
                         sog.append(play)
 
-        # print(json.dumps(sog, indent=4, separators=(',', ':')))
+        print(json.dumps(sog, indent=4, separators=(',', ':')))
+        # Interested in using the x and y coordinates to determine shot quality
+        # Want consistent continuous variables
+        # TODO: Explore using secondary type? (tip-in, snap shot, etc.
         for shot in sog:
             if "coordinates" in shot:
                 if "x" in shot["coordinates"] and "y" in shot["coordinates"]:
@@ -35,13 +44,19 @@ def pull_shots():
                     result.append(shot["result"]["eventTypeId"])
         print(str(i).zfill(4))
         print(len(result))
+        # Return a data frame
     return pd.DataFrame(list(zip(x, y, result)), columns=['x', 'y', 'result'])
 
 
 if __name__ == '__main__':
-    # shot_df = pull_shots()
-    # shot_df.to_csv("shot_data.csv", index=False)
-    shot_df = pd.read_csv('shot_data.csv')
+    # Check if the data has been pulled already
+    if os.path.exists("shot_data.csv"):
+        shot_df = pd.read_csv('shot_data.csv')
+    # If not pull it and save it to a CSV
+    else:
+        shot_df = pull_shots()
+        shot_df.to_csv("shot_data.csv", index=False)
+    
     sns.scatterplot(data=shot_df, x='x', y='y', hue='result')
     plt.show()
 
